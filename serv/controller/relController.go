808ab1327593244d8c7b69a45a994911d33707e9
AFootGolf9 <adriandosanjos000@gmail.com>
2025-06-22 11:05:58 -0300

    rel permissions

--- serv/controller/relController.go
+++ serv/controller/relController.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"stockpille/entity"
 	"stockpille/repository"
+	"stockpille/service"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,26 +26,141 @@ func GetItemQuantity(c *gin.Context) {
 }
 
 func RelAllocByUser(c *gin.Context) {
+	user := c.MustGet("user").(entity.User)
+
+	permission, err := service.GetRolePermission(user.RoleId, "relatorio1")
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "Internal server error",
+		})
+		return
+	}
+	if permission == "" {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
+	if !strings.Contains(permission, "R") {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
 	out := repository.RelAllocByUser()
 	c.JSON(200, out)
 }
 
 func RelAllocByItem(c *gin.Context) {
+	user := c.MustGet("user").(entity.User)
+
+	permission, err := service.GetRolePermission(user.RoleId, "relatorio2")
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "Internal server error",
+		})
+		return
+	}
+	if permission == "" {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
+	if !strings.Contains(permission, "R") {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
 	out := repository.RelAllocByItem()
 	c.JSON(200, out)
 }
 
 func RelItemByLocation(c *gin.Context) {
+	user := c.MustGet("user").(entity.User)
+
+	permission, err := service.GetRolePermission(user.RoleId, "relatorio3")
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "Internal server error",
+		})
+		return
+	}
+	if permission == "" {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
+	if !strings.Contains(permission, "R") {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
 	out := repository.RelItemByLocation()
 	c.JSON(200, out)
 }
 
 func RelItemByCategory(c *gin.Context) {
+	user := c.MustGet("user").(entity.User)
+
+	permission, err := service.GetRolePermission(user.RoleId, "relatorio4")
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "Internal server error",
+		})
+		return
+	}
+	if permission == "" {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
+	if !strings.Contains(permission, "R") {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
 	out := repository.RelItemByCategory()
 	c.JSON(200, out)
 }
 
 func RelUserByRole(c *gin.Context) {
+	user := c.MustGet("user").(entity.User)
+
+	permission, err := service.GetRolePermission(user.RoleId, "relatorio5")
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": "Internal server error",
+		})
+		return
+	}
+	if permission == "" {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
+	if !strings.Contains(permission, "R") {
+		c.JSON(403, gin.H{
+			"error": "Forbidden",
+		})
+		return
+	}
+
 	out := repository.RelUserByRole()
 	c.JSON(200, out)
 }
